8afa7a7a6fcfe075d406a1bdd86eeaa19ed54e45
John Seong <wonmor@gmail.com>
2022-02-16 16:34:38 -0500

    Update S1.py

--- CCC/2022/S1.py
+++ CCC/2022/S1.py
@@ -1,3 +1,5 @@
+# Thank you, Mr. Ben Grossmann from StackOverFlow, for contributing in this project!
+
 import math
 
 inputNum = int(input()); print(max(0, 1 + inputNum // 4 - math.ceil(inputNum / 5)))
